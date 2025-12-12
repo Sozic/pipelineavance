b018145499f0ce94cabd815020939d43e9793f4b
Sozic <90249016+Sozic@users.noreply.github.com>
2025-12-12 15:48:24 +0100

    Refactor main function for better parameter handling
    Refactor main function to handle single and dual parameter operations separately. Improve error handling for parameter count.

--- src/main.c
+++ src/main.c
@@ -3,37 +3,39 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "calculator") == 0) {
-		r = _calculator(atof(a));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Erreur de parametres");
+        return 0;
+    }
 
-		
+    char* op = argv[1];
+    double r = 0;
+
+    /* opérations à 1 paramètre */
+    if (strcmp(op, "car") == 0) {
+        if (argc != 3) { printf("Erreur de parametres"); return 0; }
+        r = _car(atof(argv[2]));
+        printf("%lf", r);
+        return 0;
+    }
+
+    /* opérations à 2 paramètres */
+    if (argc != 4) {
+        printf("Erreur de parametres");
+        return 0;
+    }
+
+    char* a = argv[2];
+    char* b = argv[3];
+
+    if (strcmp(op, "add") == 0) r = _add(atof(a), atof(b));
+    else if (strcmp(op, "sub") == 0) r = _sub(atof(a), atof(b));
+    else if (strcmp(op, "mul") == 0) r = _mul(atof(a), atof(b));
+    else if (strcmp(op, "div") == 0) r = _div(atof(a), atof(b));
+    else { printf("Erreur de parametres"); return 0; }
+
+    printf("%lf", r);
+    return 0;
+}
+    
